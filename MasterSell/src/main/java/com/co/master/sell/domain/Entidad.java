47dbbfc72beb5f3e52236a2ec45303e4916fabb6
Teban-Ing <Teban-Ing@DESKTOP-JGJGNRN>
2016-06-20 21:27:49 -0500

    MasterSell - Davigofr - Se crea clase de prueba

---
+++ MasterSell/src/main/java/com/co/master/sell/domain/Entidad.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.co.master.sell.domain;
+
+/**
+ *
+ * @author Teban-Ing
+ */
+public class Entidad {
+    
+}
